ad88047b2846c0ca715a2cf6939fd798f73aab77
ettylax <ettylax@gmail.com>
2025-03-11 16:54:05 +0200

    add founcget

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import express from 'express';
+
+const app = express();
+const port = 5000;
 
 const options = {
   method: 'GET',
@@ -13,3 +17,17 @@ axios
   .request(options)
   .then(res => console.log(res.data))
   .catch(err => console.error(err));
+  
+  app.get('/', (req, res) => {
+    axios
+      .request(options)
+      .then(response => {
+        res.json(response.data); 
+      })
+      .catch(error => {
+        res.status(500).json({ error: 'Error fetching data from Render API' });
+      });
+  });
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
